450ce4aae9bcb9a712f161fa6c2344f7c1d560d6
Nathan Osman <nathan@quickmediasolutions.com>
2025-07-12 21:51:53 -0700

    Wait for SIGINT or SIGTERM in main().

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/urfave/cli/v2"
 )
@@ -11,6 +13,15 @@ func main() {
 	app := &cli.App{
 		Name:  "nitroshare2",
 		Usage: "cross-platform file transfer application",
+		Action: func(c *cli.Context) error {
+
+			// Wait for SIGINT or SIGTERM
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+			<-sigChan
+
+			return nil
+		},
 	}
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
